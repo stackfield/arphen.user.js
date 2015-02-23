ad52a0509c97a4929d1f09d08e2615541ea7e5f1
Arphen Lin <arphenlin@gmail.com>
2015-02-23 18:01:09 +0800

    create eyny.user.js
    https://openuserjs.org/scripts/arphenlingmail.com/Eyny_Cleaner

---
+++ eyny.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name          Eyny Cleaner
+// @namespace     https://arphen.github.io/
+// @version       0.1.20150223
+// @description   Hide some elements
+// @include       http://*.eyny.com/*
+// @copyright     2015, Arphen Lin
+// @author     Arphen Lin
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+// @grant         none
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+//var url = window.location.href;
+
+$('div#hd + table[width], td.forumlist').hide();
+$('div#pt, div#ft, div#pgt, div.hdc, div#f_pst, div.bm.bml.pbn, ul#thread_types').hide();
+$('h2 a img').hide();
+$('table[height="240"], table.t_p_top').hide();
