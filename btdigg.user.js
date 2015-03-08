0a3237697ff4fd3be2f883c5abc032334c3f8f7c
Arphen Lin <arphenlin@gmail.com>
2015-03-08 21:21:53 +0800

    update

--- btdigg.user.js
+++ btdigg.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name          BTDigg Cleaner
+// @name          BTDigg
 // @namespace     https://arphen.github.io/
-// @version       0.1.20150227
-// @description   Hide some elements
+// @version       0.2.20150308
+// @description   Do something
 // @include       http*://btdigg.org/*
 // @copyright     2015+, Arphen Lin
 // @author     Arphen Lin
@@ -13,3 +13,20 @@
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 $('#logo').hide();
+
+// click to copy magnet link
+/*
+$('a[title*="magnet"]').each(function(index){
+    var magnet = $(this).attr('href');
+    $(this).after('<a class="copylink" link="'+magnet+'">[Copy Link]</a>');
+});
+*/
+
+$('a[title*="magnet"]').click(function(){
+    copyToClipboard( $(this).attr('href') );
+});
+
+function copyToClipboard(text) {
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+}
+
