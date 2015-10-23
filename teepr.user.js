94a27cf1b6d1cc4ed662d9fd087e694dc4c1b8aa
Arphen Lin <arphenlin@gmail.com>
2015-10-23 18:40:24 +0800

    new

---
+++ teepr.user.js
@@ -0,0 +1,22 @@
+﻿// ==UserScript==
+// @name          Teepr
+// @namespace     https://arphen.github.io/
+// @version       0.1.20151023
+// @description   As I wish
+// @include       http://www.teepr.com/*
+// @copyright     2015+, Arphen Lin
+// @author        Arphen Lin
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @grant         none
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+function doit(){
+    $('.main-header, .secondary-navigation, #top-navigation, .sidebar, #content_box header, .copyrights').remove();
+    $('.wf-formTpl, .wf-formTpl~*, .post~*').remove();
+    $('#popmake-overlay, div.popmake').remove();
+    //setTimeout(doit, 1000);
+};
+
+doit();
