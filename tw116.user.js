2e310dcc73ebf88bd257307fea898f9368049386
Arphen Lin <arphenlin@gmail.com>
2015-11-09 21:45:33 +0800

    tw116

---
+++ tw116.user.js
@@ -0,0 +1,50 @@
+﻿// ==UserScript==
+// @name          TW116
+// @namespace     https://arphen.github.io/
+// @version       0.1.20151109
+// @description   As I wish
+// @include       http://www.tw116.com/vod-show-id-*
+// @copyright     2015+, Arphen Lin
+// @author        Arphen Lin
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @grant         none
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+function log(text){
+    try{
+        console.log('[TW116] ' + text);
+    }catch(err){
+    }
+}
+
+function goto(page){
+    $('a.pagegbk').each(function(){
+        var s = $(this).text();
+        if(s == page){
+            log(s);
+            this.click();
+            return false; // break .each() loop
+        }
+    });                    
+}
+
+function doit(){
+    $('#logo, div.htop, div.tbpic, #center, #footer').remove();
+
+    $('body').keydown(function(e){
+        log('keydown = ' + e.which);
+        //return;
+        switch(e.which){
+            case 39: // right key -> next page
+                goto("下一頁");
+                break;
+            case 37: // left key -> prev page
+                goto("上一頁");
+                break;
+        }
+    });
+};
+
+doit();
