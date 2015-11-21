5145d28b8085c69eb894546f7b133e36fb2355c2
Arphen Lin <arphenlin@gmail.com>
2015-11-21 19:12:21 +0800

    Update tw116.user.js

--- tw116.user.js
+++ tw116.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          TW116
 // @namespace     https://github.com/arphen/arphen.user.js
-// @version       0.9.20151121
+// @version       1.0.20151121
 // @description   As I wish
 // @include       http://www.tw116.com/*
 // @copyright     2015+, Arphen Lin
@@ -32,8 +32,49 @@ function goto(page){
     });                    
 }
 
+var VideoList = [
+    // anime
+    "全部成爲F",
+    "偵探小隊KZ",
+    "青年黑傑克",
+    "櫻子小姐的腳下",
+    "野良神第二季",
+    // TV
+    "僞裝夫妻",
+    "熟女正青春",
+    "下町火箭",
+    "掟上今日子的備忘錄",
+    "武士老師",
+    "孤獨的美食家第五季",
+    "遺産争族",
+    "診斷之眼",
+    "相棒第十四季",
+    // end
+    "EENNDD"
+];
+
+function hilite(){
+    $('div.mlist dl dt a').each(function(){
+        var s = $(this).text();
+        //log(s);
+
+        //var re = new RegExp("(偵探小隊|黑傑克)");
+        var re = new RegExp(VideoList.join("|"));
+
+        //if(s.search(/(偵探小隊|黑傑克)/)>=0){
+        if(re.test(s)){
+            var o = $(this).parent().parent().parent();
+            $(o).css({backgroundColor: "#550055"}); // set back color
+        }     
+    });
+};
+
+
 function doit(){
     $('#logo, div.htop, div.tbpic, #center, #footer, #t365').remove();
+
+    // hilite
+    hilite();
 
     $('body').keydown(function(e){
         log('keydown = ' + e.which);
