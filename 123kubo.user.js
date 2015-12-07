2e9c7a8e35152448b3b0e80a1434dfd41b656ac4
Arphen Lin <arphenlin@gmail.com>
2015-12-07 19:23:25 +0800

    Create 123kubo.user.js

---
+++ 123kubo.user.js
@@ -0,0 +1,98 @@
+// ==UserScript==
+// @name          123KUBO
+// @namespace     https://github.com/arphen/arphen.user.js
+// @version       0.1.20151207
+// @description   As I wish
+// @include       http://www.123kubo.com/*
+// @copyright     2015+, Arphen Lin
+// @author        Arphen Lin
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @grant         none
+// ==/UserScript==
+/* jshint -W097 */
+//'use strict';
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+var VideoList = [
+    // anime
+    "全部成爲F",
+    "偵探小隊KZ",
+    "青年黑傑克",
+    "櫻子小姐的腳下",
+    "野良神第二季",
+    "魯邦三世新系列",
+    "終物語",
+    // TV
+    "僞裝夫妻",
+    "熟女正青春",
+    "下町火箭",
+    "掟上今日子的備忘錄",
+    "武士老師",
+    "孤獨的美食家第五季",
+    "遺産争族",
+    "診斷之眼",
+    "相棒第十四季",
+    "TransitGirls",
+    "芈月傳",
+    // end
+    "=== END ==="
+];
+
+function log(text){
+    try{
+        console.log('[123KUBO] ' + text);
+    }catch(err){
+    }
+}
+
+function goto(page){
+    $('a.pagegbk').each(function(){
+        var s = $(this).text();
+        if(s == page){
+            log(s);
+            this.click();
+            return false; // break .each() loop
+        }
+    });
+}
+
+function hilite(){
+    $('div.listlf ul li p.t a').each(function(){
+        var s = $(this).text();
+        log(s);
+
+        //var re = new RegExp("(偵探小隊|黑傑克)");
+        var re = new RegExp(VideoList.join("|"));
+
+        //if(s.search(/(偵探小隊|黑傑克)/)>=0){
+        if(re.test(s)){
+            var o = $(this).parent().parent();
+            $(o).css({backgroundColor: "#550055",
+                      color: "#ccc"}); // set color
+        }
+    });
+}
+
+
+function main(){
+    $('div.listforum, div.footer, div.datal.clear, ul.ad').remove();
+
+    // hilite
+    hilite();
+
+    $('body').keydown(function(e){
+        log('keydown = ' + e.which);
+        //return;
+        switch(e.which){
+            case 39: // right key -> next page
+                goto("下一頁");
+                break;
+            case 37: // left key -> prev page
+                goto("上一頁");
+                break;
+        }
+    });
+}
+
+main();
