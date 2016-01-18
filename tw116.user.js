c1696d13ece8cb82bfe9f71e054bf7fea2606979
Arphen Lin <arphenlin@gmail.com>
2016-01-18 09:52:09 +0800

    Update tw116.user.js

--- tw116.user.js
+++ tw116.user.js
@@ -1,47 +1,17 @@
 // ==UserScript==
 // @name          TW116
 // @namespace     https://github.com/arphen/arphen.user.js
-// @version       2.2.20160117
+// @version       2.3.20160118
 // @description   As I wish
 // @include       http://www.tw116.com/*
 // @copyright     2015+, Arphen Lin
 // @author        Arphen Lin
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js
+// @require       http://arphen.github.io/user_script_libs/video.js
 // @grant         none
 // ==/UserScript==
-// http://arphen.github.io/user_script_libs/video.js
-
 
 this.$ = this.jQuery = jQuery.noConflict(true);
-
-var VideoList = [
-	// anime
-	"暗殺教室",
-	"排球少年",
-	"全部成爲F",
-	"偵探小隊KZ",
-	"黑傑克",
-	"櫻子小姐的腳下",
-	"野良神第二季",
-	"魯邦三世",
-	"終物語",
-	// TV
-	"福爾摩斯",
-	"黑名單",
-	"僞裝夫妻",
-	"熟女正青春",
-	"下町火箭",
-	"掟上今日子的備忘錄",
-	"武士老師",
-	"孤獨的美食家",
-	"遺産争族",
-	"相棒",
-	"TransitGirls",
-	"芈月傳",
-	"鄉村愛情",
-	// end
-	"=== END ==="
-];
 
 function log(text){
 	try{
