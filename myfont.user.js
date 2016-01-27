c302dde8625eaaf4fc9547df4e93606fe95d80b8
Arphen Lin <arphenlin@gmail.com>
2016-01-27 23:30:46 +0800

    Update myfont.user.js

--- myfont.user.js
+++ myfont.user.js
@@ -2,7 +2,7 @@
 // @name         MyFont
 // @namespace    https://github.com/arphen/arphen.user.js/blob/master/myfont.user.js
 // @homepage     https://github.com/arphen/arphen.user.js/blob/master/myfont.user.js
-// @version      0.9.20160127
+// @version      1.0.20160127
 // @description  use my font
 // @copyright    2016+, Arphen Lin
 // @author       Arphen Lin
@@ -17,6 +17,7 @@
 // @exclude      *taiwanmooc.org*
 // @exclude      *coursera.*
 // @exclude      *edx.org*
+// @exclude      *openedu.*
 // @grant        GM_addStyle
 // ==/UserScript==
 
