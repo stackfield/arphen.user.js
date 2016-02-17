30df693df11236144a9dc9c1fa238322c96c2c2d
Arphen Lin <arphenlin@gmail.com>
2016-02-17 22:17:09 +0800

    Update tw116.user.js

--- tw116.user.js
+++ tw116.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          TW116
 // @namespace     https://github.com/arphen/arphen.user.js/blob/master/tw116.user.js
-// @version       2.5.20160215
+// @version       2.6.20160217
 // @description   As I wish
 // @include       http://www.tw116.com/*
 // @copyright     2015+, Arphen Lin
@@ -21,6 +21,14 @@ function log(text){
 	}catch(err){
 	}
 }
+
+function addScript(url) {
+	var scriptElement = document.createElement( "script" );
+	scriptElement.type = "text/javascript";
+	scriptElement.src = url;
+	document.body.appendChild( scriptElement );
+}
+
 
 function goto(page){
 	$('a.pagegbk').each(function(){
@@ -49,6 +57,61 @@ function hilite(){
 	});
 }
 
+/*
+var win;
+function getRealURL(obj, pwd, href){
+	var id = 'ifm_' + pwd;
+	//win = window.open(href, "_blank", "location=yes,height=570,width=520,scrollbars=yes,status=yes");
+	win = window.open(href, "_blank");
+	setInterval(function(){
+		log('postMessage(): ' + pwd);
+		win.postMessage(pwd, 'http://pan.baidu.com');
+		//alert(win.location.href); // 會因安全性的問題被block => javascript - SecurityError: Blocked a frame with origin from accessing a cross-origin frame - http://goo.gl/h29eh8
+	}, 3000);
+	return href;
+}
+
+function getRealURL_iframe(obj, pwd, href){
+	var id = 'ifm_' + pwd;
+	$(obj).append('<br><iframe id="' +id+ '" width="1000" height="350" scrolling="yes" src="' +href+ '"></iframe>');
+	//var url = document.frames[id].location.href;
+	//log(url);
+	$('#'+id).load(function(){
+		//alert('url = ' + this.contentWindow.location.href); // 會因安全性的問題被block => javascript - SecurityError: Blocked a frame with origin from accessing a cross-origin frame - http://goo.gl/h29eh8
+		this.contentWindow.postMessage(pwd, 'http://www.tw116.com/');
+	});
+	return href;
+}
+
+var ajax =
+	{
+		send: function(urlstring)
+		{
+			if(!this.ifram)
+			{
+				this.ifram = document.createElement('iframe');
+				//this.ifram.style.display = 'none';
+				if(this.ifram.addEventListener) this.ifram.addEventListener('load',ajax.receive,false);
+				else if(this.ifram.attachEvent) this.ifram.attachEvent('onload',ajax.receive);
+				//$(this.ifram).load(ajax.receive);
+				document.body.appendChild(this.ifram);
+			}
+			this.ifram.setAttribute('src',urlstring);
+		},
+		receive: function()
+		{
+			content = ajax.ifram.contentWindow.document.body.innerHTML;
+			returnurl = ajax.ifram.contentWindow.location.href; // 此時根本還沒載入, about:blank
+			log('return url: '+returnurl);
+			setTimeout(ajax.receive_delay, 5000); // 等5秒載入後再抓網址
+		},
+		receive_delay: function(){
+			returnurl = ajax.ifram.contentWindow.location.href; // 會因安全性的問題被block => javascript - SecurityError: Blocked a frame with origin from accessing a cross-origin frame - http://goo.gl/h29eh8
+			log('return url: '+returnurl);
+		}
+	};
+*/
+
 function baiduLink(){
 	//$('div.movie a[href~="baidu"]').each(function(){
 	$('div.movie a[href^="http://pan.baidu.com"]').each(function(){
@@ -56,23 +119,49 @@ function baiduLink(){
 		log(s);
 		if(s.indexOf('密碼') > 0){
 			s = s.trim();
-			acc_code = s.substr(s.length-4, 4);
-			log('密碼=' + acc_code);
+			pwd = s.substr(s.length-4, 4);
+			log('密碼=' + pwd);
 			var href = $(this).attr('href');
-			$(this).attr("href", href + '&acc_code=' + acc_code); // todo: 百度縮址加上acc_code沒用! 要設法取得真網址
+			//var url = getRealURL(this, pwd, href);
+			//ajax.send(href); // todo: not work
+			//$(this).attr("href", href + '&pwd=' + pwd); // todo: 百度縮址加上pwd沒用! 要設法取得真網址
+			$(this).removeAttr('href')
+				.attr('id', 'pwd_'+pwd)
+				.attr('title', href)
+				.attr('onclick', 'inpage_openBaiduPan(this)');
+			//$(this).after('<a id="pwd_'+pwd+'" title="'+href+'" onclick="inpage_openBaiduPan(this)">Baidu</a>');
 		}
 	});
 }
 
+/* this code is for page only */
+function inpage_openBaiduPan(){
+	var a = event.target;
+	//alert(a.title);
+	var pwd = a.id.replace("pwd_", "");
+	win = window.open(a.title, "_blank");
+	setInterval(function(){
+		win.postMessage(pwd, 'http://pan.baidu.com');
+	}, 2000);
+}
+
 
 function main(){
-	//$('#logo, div.htop, div.tbpic, #center, #footer, #t365').remove();
+	// inject jquery in page
+	addScript('http://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js');
+
+	// inject my code in page
+	var script = document.createElement('script');
+	script.appendChild(document.createTextNode('var win; ' + inpage_openBaiduPan));
+	(document.body || document.head || document.documentElement).appendChild(script);
 
 	// hilite
 	hilite();
 
+	// process baidu pan
 	baiduLink();
 
+	// add short-key
 	$('body').keydown(function(e){
 		log('keydown = ' + e.which);
 		//return;
