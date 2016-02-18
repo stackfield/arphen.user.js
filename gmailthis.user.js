5444a9eba4def4f3c64800f2bb323a9bead5fb41
Arphen Lin <arphenlin@gmail.com>
2016-02-18 23:00:33 +0800

    Create gmailthis.user.js

---
+++ gmailthis.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name          GMail This
+// @namespace     https://github.com/arphen/arphen.user.js/blob/master/gmailthis.user.js
+// @version       1.0.20160218
+// @description   As I wish
+// @include       https://mail.google.com/mail/u/*
+// @copyright     2016+, Arphen Lin
+// @author        Arphen Lin
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.js
+// @require       http://arphen.github.io/user_script_libs/utility.js
+// @grant         none
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+function insertMyEmail(){
+	myLog.log('insertMyEmail');
+	if($("textarea#:oj.vO").length === 0){
+		setTimeout(insertMyEmail, 3000); // wait and try again
+		return;
+	}else{
+		var me = '<div id="myEmail" class="vR"><span class="vN Y7BVp a3q" email="arphen@cht.com.tw"><div class="vT">681林佳宏</div><div class="vM"></div></span><input name="to" type="hidden" value="681林佳宏 <arphen@cht.com.tw>"><input name="notlsindicator" type="hidden" value="arphen@cht.com.tw"></div>';
+		$("textarea#:oj.vO").before(me);
+	}
+}
+
+function sendMail(){
+	myLog.log('ctrlEnter: ' + $('div[data-tooltip^="Send"]').length);
+	$('div[data-tooltip^="Send"]')[0].click();
+}
+
+function main(){
+	// use myLog
+	myLog.init('GMail This');
+
+	setInterval(sendMail, 2000);
+}
+
+main();
