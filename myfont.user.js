04b34c652b466e7d1e0602f968191817acd85314
Arphen Lin <arphenlin@gmail.com>
2016-03-09 10:52:21 +0800

    Update myfont.user.js

--- myfont.user.js
+++ myfont.user.js
@@ -18,16 +18,21 @@
 // @exclude      *edx.org*
 // @exclude      *openedu.*
 // @exclude      *jsfiddle.*
-// @exclude      *livehouse.in*
+// @exclude      *211.20.181.194*
+// @require       https://rawgit.com/arphen/arphen.user.js/master/libs/utility.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
 	'use strict';
 
-	// Your code here...
 	//var css = '* { font-family: ebrima, gadugi, "lao ui", "leelawadee ui", "meiryo ui", "segoe ui", Arial, Meiryo, "microsoft jhenghei ui", "微軟正黑體", "microsoft yahei", "microsoft yahei ui" !important; }';
-	var css = '* { font-family: Tahoma, Helvetica, Arial, "segoe ui", "microsoft jhenghei" !important; }  pre, pre > * {font-family: consolas !important; } ';
+	//var css = '* { font-family: Helvetica, tahoma, arial, "segoe ui", "microsoft jhenghei" !important; }  pre, pre > * {font-family: consolas !important; } ';
+
+	// Google Fonts 推出「思源黑體」中文網頁字型，改善網頁文字顯示效果 - https://goo.gl/yuGglm
+	APLTOOL.loadFile('http://fonts.googleapis.com/earlyaccess/notosanstc.css', 'css');
+	var css = '* { font-family:  Helvetica, tahoma, arial, "segoe ui", "Noto Sans TC", "microsoft jhenghei" !important; }  pre, pre > * {font-family: consolas !important; } ';
+	//var css = '* { font-family:  "Noto Sans TC" !important; }  pre, pre > * {font-family: consolas !important; } ';
 
 	GM_addStyle(css);
 })();
