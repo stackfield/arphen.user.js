480eb8e5a8ba099c511752969ad75f62adcd4b55
Arphen Lin <arphenlin@gmail.com>
2016-05-30 16:28:09 +0800

    Update autocomplete.user.js

--- autocomplete.user.js
+++ autocomplete.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Autocomplete
 // @namespace     https://github.com/arphen/arphen.user.js/blob/master/autocomplete.user.js
-// @version       1.1.20160530
+// @version       1.2.20160530
 // @description   As I wish
 // @include       http*://*
 // @exclude       *ebank.bot.com.tw*
@@ -21,16 +21,23 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 var url = window.location.href.toLowerCase();
 
 function main() {
-    myLog.init('Autocomplete');
+  myLog.init('Autocomplete');
 
-    GM_addStyle('.ui-helper-reset,.ui-menu{list-style:none;padding:0;outline:0}.ui-helper-hidden{display:none}.ui-helper-hidden-accessible{border:0;clip:rect(0 0 0 0);height:1px;margin:-1px;overflow:hidden;padding:0;position:absolute;width:1px}.ui-helper-zfix,.ui-widget-overlay{width:100%;height:100%;left:0;top:0}.ui-helper-reset{margin:0;border:0;line-height:1.3;text-decoration:none;font-size:100%}.ui-helper-clearfix:after,.ui-helper-clearfix:before{content:"";display:table;border-collapse:collapse}.ui-helper-clearfix:after{clear:both}.ui-helper-clearfix{min-height:0}.ui-helper-zfix{position:absolute;opacity:0;filter:Alpha(Opacity=0)}.ui-front{z-index:100}.ui-state-disabled{cursor:default!important}.ui-icon{display:block;text-indent:-99999px;overflow:hidden;background-repeat:no-repeat}.ui-widget-overlay{position:fixed}.ui-autocomplete,.ui-menu .ui-menu{position:absolute}.ui-autocomplete{top:0;left:0;cursor:default}.ui-menu{margin:0;display:block}.ui-menu .ui-menu-item{position:relative;margin:0;padding:3px 1em 3px .4em;cursor:pointer;min-height:0;list-style-image:url(data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7)}.ui-menu .ui-menu-divider{margin:5px 0;height:0;font-size:0;line-height:0;border-width:1px 0 0}.ui-menu-icons{position:relative}.ui-menu-icons .ui-menu-item{padding-left:2em}.ui-menu .ui-icon{position:absolute;top:0;bottom:0;left:.2em;margin:auto 0}.ui-menu .ui-menu-icon{left:auto;right:0}');
-    GM_addStyle('.ui-autocomplete{padding:0;list-style:none;background-color:#fff;width:218px;border:1px solid #B0BECA;max-height:350px;overflow-x:hidden}.ui-autocomplete .ui-menu-item{border-top:1px solid #B0BECA;display:block;padding:4px 6px;color:#353D44;cursor:pointer}.ui-autocomplete .ui-menu-item:first-child{border-top:none}.ui-autocomplete .ui-menu-item.ui-state-focus{background-color:#D5E5F4;color:#161A1C}');
+  GM_addStyle('.ui-helper-reset,.ui-menu{list-style:none;padding:0;outline:0}.ui-helper-hidden{display:none}.ui-helper-hidden-accessible{border:0;clip:rect(0 0 0 0);height:1px;margin:-1px;overflow:hidden;padding:0;position:absolute;width:1px}.ui-helper-zfix,.ui-widget-overlay{width:100%;height:100%;left:0;top:0}.ui-helper-reset{margin:0;border:0;line-height:1.3;text-decoration:none;font-size:100%}.ui-helper-clearfix:after,.ui-helper-clearfix:before{content:"";display:table;border-collapse:collapse}.ui-helper-clearfix:after{clear:both}.ui-helper-clearfix{min-height:0}.ui-helper-zfix{position:absolute;opacity:0;filter:Alpha(Opacity=0)}.ui-front{z-index:100}.ui-state-disabled{cursor:default!important}.ui-icon{display:block;text-indent:-99999px;overflow:hidden;background-repeat:no-repeat}.ui-widget-overlay{position:fixed}.ui-autocomplete,.ui-menu .ui-menu{position:absolute}.ui-autocomplete{top:0;left:0;cursor:default}.ui-menu{margin:0;display:block}.ui-menu .ui-menu-item{position:relative;margin:0;padding:3px 1em 3px .4em;cursor:pointer;min-height:0;list-style-image:url(data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7)}.ui-menu .ui-menu-divider{margin:5px 0;height:0;font-size:0;line-height:0;border-width:1px 0 0}.ui-menu-icons{position:relative}.ui-menu-icons .ui-menu-item{padding-left:2em}.ui-menu .ui-icon{position:absolute;top:0;bottom:0;left:.2em;margin:auto 0}.ui-menu .ui-menu-icon{left:auto;right:0}');
+  GM_addStyle('.ui-autocomplete{padding:0;list-style:none;background-color:#fff;width:218px;border:1px solid #B0BECA;max-height:350px;overflow-x:hidden}.ui-autocomplete .ui-menu-item{border-top:1px solid #B0BECA;display:block;padding:4px 6px;color:#353D44;cursor:pointer}.ui-autocomplete .ui-menu-item:first-child{border-top:none}.ui-autocomplete .ui-menu-item.ui-state-focus{background-color:#D5E5F4;color:#161A1C}');
 
-    //APLTOOL.addScriptCode(inpage_autocomplete + ' setTimeout(inpage_autocomplete, 5000);');
-    waitForKeyElements("select:not([multiple])", function() {
-        //debugger;
-        $('select:not([multiple])').selectToAutocomplete();
+  //APLTOOL.addScriptCode(inpage_autocomplete + ' setTimeout(inpage_autocomplete, 5000);');
+  waitForKeyElements("select:not([multiple])", function() {
+    //$('select:not([multiple])').selectToAutocomplete();
+    $('select:not([multiple])').each(function() {
+      var opts = $(this).find('option');
+      myLog.log(opts.length);
+      //debugger;
+      if (opts.length > 10) { // 超過才變為自動
+        $(this).selectToAutocomplete();
+      }
     });
+  });
 }
 
 main();
