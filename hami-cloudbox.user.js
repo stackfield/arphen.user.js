149161b400bbe629bf008603e5a209c82dabf0d8
Arphen Lin <arphenlin@gmail.com>
2016-06-05 00:16:04 +0800

    Update hami-cloudbox.user.js

--- hami-cloudbox.user.js
+++ hami-cloudbox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Hami Cloudbox
 // @namespace     https://github.com/arphen/arphen.user.js/blob/master/hami-cloudbox.user.js
-// @version       1.0.20160604
+// @version       1.1.20160604
 // @description   As I wish
 // @include       http://sync.hamicloud.net/*
 // @copyright     2015+, Arphen Lin
@@ -15,10 +15,8 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 
 function selectDeleted(){
 	// 自動勾選已刪除檔案
-	$('td.back-row-name a[data-type=0]').each(function(){
-		var tr = this.parentNode.parentNode;
-		var chk = $('input.cbox', tr);
-		$(chk[0]).prop( "checked", true );
+	$('tr.back-row-deleted input.cbox').each(function(){
+		$(this).prop( "checked", true );
 	});
 }
 
