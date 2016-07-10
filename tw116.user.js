23c5061db1cc7f2bbf8d667d1f6ffe307e841af3
Arphen Lin <arphenlin@gmail.com>
2016-07-10 17:25:35 +0800

    Update tw116.user.js

--- tw116.user.js
+++ tw116.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          TW116
 // @namespace     https://github.com/arphen/arphen.user.js/blob/master/tw116.user.js
-// @version       3.4.20160710
+// @version       3.5.20160710
 // @description   As I wish
 // @include       http://www.tw116.com/*
 // @copyright     2015+, Arphen Lin
